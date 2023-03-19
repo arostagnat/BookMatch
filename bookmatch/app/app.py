90fa5bc58ef65cfcc426c458d91da0d6f786a622
jeffrey dejax <jeffrey.dejax@michelin.com>
2023-03-19 08:48:31 +0100

    push du tuning

--- bookmatch/app/app.py
+++ bookmatch/app/app.py
@@ -7,11 +7,11 @@ import time
 from bookmatch.params import *
 from pathlib import Path
 
-
 local_raw_data_path = Path(__file__).parents[2].joinpath("data/raw_data")
 file_path = Path(local_raw_data_path).joinpath("raw_movies/metadata.json")
 metadata_movies = pd.read_json(file_path,lines = True)
 openai.api_key=OPENAI_API_KEY
+
 
 
 st.set_page_config(
@@ -31,17 +31,67 @@ You tell us your favorite :red[films] 🎬 We tell you the :blue[books] to read 
 def get_movie_data():
     return metadata_movies["title"]
 
+
+prediciton=None
+
 with st.form(key='params_for_api'):
 
     movie_title_selection = get_movie_data()
 
     movie_titles = st.multiselect("What are some of your favorite movies?",
                                  movie_title_selection,max_selections=10) ### TODO remove 10 max selections and make the choice less laggy
-
-
     if st.form_submit_button('Get my books'):
 
         movie_ids = [metadata_movies[metadata_movies.title == title].item_id.values[0] for title in movie_titles]
+
+        for i, e in enumerate(movie_ids):
+            movie_ids[i] = str(e)
+
+        movie_ids_list = '$$$$$'.join(movie_ids)
+
+        # Add API url below
+        bookmatch_url = 'http://localhost:8000/predict'
+        response = requests.get(bookmatch_url, params={"movie_list":movie_ids_list})
+
+        global prediction
+        prediction = response.json()
+
+        st.session_state["pred"]=prediction
+
+        with st.spinner('Searching for your recommendations...'):
+            time.sleep(4)
+
+            if prediction.get("book_list"):
+                st.markdown(f"### Your :blue[book] recommendations are:")
+                for book in prediction["book_list"]:
+                    st.markdown(f'#### {book}')
+
+    if st.form_submit_button('Ask GPT'):
+        with st.spinner("**:red[Chat GPT]** is generating an explanation..."):
+
+            prediction=st.session_state["pred"]
+
+            ### Chat GPT comment
+            chat_input = f"""Someone enjoyed watching the movies {movie_titles}, write a 3-sentence paragraph explaining why this person might
+            enjoy reading {prediction["book_list"]}"""
+
+            completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": chat_input}])
+
+            st.write(completion.choices[0].message["content"])
+
+            st.write("""
+            <p> <a href="https://youtu.be/ws3WGmINlIg?t=14">🍔</a>
+            </p>
+        """,unsafe_allow_html = True)
+
+    if st.form_submit_button('Example 1'):
+
+        movie_ids=["4896","40815", "88125", "93006"] #exemple harry potter
+        # movie_ids=["1"]
+        # movie_ids = [metadata_movies[metadata_movies.title == title].item_id.values[0] for title in movie_titles]
 
         for i, e in enumerate(movie_ids):
             movie_ids[i] = str(e)
@@ -78,3 +128,6 @@ with st.form(key='params_for_api'):
          <p> <a href="https://youtu.be/ws3WGmINlIg?t=14">🍔</a>
          </p>
         """,unsafe_allow_html = True)
+
+
+        pass
